225f92d9749634ad2538658225c29f4797cd5f1d
Nirmal <nirmalchandran06@gmail.com>
2021-08-24 08:42:35 +0530

    Initial Commit

---
+++ cell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cell
+//
+//  Created by Nirmal Chandran on 24/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ cellTests/cellTests.swift
@@ -0,0 +1,33 @@
+//
+//  cellTests.swift
+//  cellTests
+//
+//  Created by Nirmal Chandran on 24/08/21.
+//
+
+import XCTest
+@testable import cell
+
+class cellTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
